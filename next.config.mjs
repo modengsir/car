825cb48383b7dd7d380a01fc77c518ca7a6fef94
modengsir <modengsir@gmail.com>
2026-03-04 23:59:29 +0800

    Update next.config.mjs for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
-
-export default nextConfig
+export default {
+  output: 'export',
+  basePath: '/car',
+};
